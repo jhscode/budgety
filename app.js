f3cae1c10448f2bda0c8379e057e5475c9616d6f
James Shin <jhscode@gmail.com>
2018-04-07 17:08:53 -0400

    added domstrings and controller add item

--- app.js
+++ app.js
@@ -6,16 +6,41 @@ var budgetController = (function() {
 // UI CONTROLLER
 var UIController = (function() {
 
+  var DOMstrings = {
+    inputType: '.add__type',
+    inputDescription: '.add__description',
+    inputValue: '.add__value',
+    inputBtn: '.add__btn'
+  }
 
+  return {
+    getInput: function() {
+      return {
+        type: document.querySelector(DOMstrings.inputType).value, // Will be either inc or exp
+        description: document.querySelector(DOMstrings.inputDescription).value,
+        value: document.querySelector(DOMstrings.inputValue).value
+      };
+    },
+
+    getDOMstrings: function() {
+      return DOMstrings;
+    }
+
+  }
 })();
 
 
-// GLOBAL APP CONTROLLER
+// GLOBAL APP
 var controller = (function(budgetCtrl, UICtrl) {
+
+  var DOM = UICtrl.getDOMstrings();
 
   var ctrlAddItem = function() {
 
         // 1. Get the filed input data
+        var input = UICtrl.getInput();
+        console.log(input);
+
 
         // 2. Add the item to the budget CONTROLLER
 
@@ -25,9 +50,9 @@ var controller = (function(budgetCtrl, UICtrl) {
 
         // 5. Display the budget on the UI
 
-  } 
+  }
 
-  document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
+  document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
 
   document.addEventListener('keypress', function(event) {
     if (event.keyCode === 13 || event.which === 13) {
