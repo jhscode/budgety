0b8c88ae75f86028125d6abd97a9b4b4db01e6e5
James Shin <jhscode@gmail.com>
2018-04-18 08:25:42 -0400

    added html string with placeholder to addlistitem function

--- app.js
+++ app.js
@@ -77,6 +77,25 @@ var UIController = (function() {
     },
 
     addListItem: function(obj, type) {
+      var html, newHTML;
+
+      // Create HTML string with placeholder text
+      if (type === 'inc') {
+        html = '<div class="item clearfix" id="income-%id%"><div class="item__description">'+
+        '%description%</div><div class="right clearfix"><div class="item__value">%value%</div>'+
+        '<div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline">'+
+        '</i></button></div></div></div>';
+
+      } else if (type === 'exp') {
+        html = '<div class="item clearfix" id="expense-%id%"><div class="item__description">%description%</div>'+
+        '<div class="right clearfix"><div class="item__value">%value%</div><div class="item__percentage">'+
+        '21%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i>'+
+        '</button></div></div></div>';
+      }
+
+      // Replace the placeholder text with actual data
+
+      // Insert HTMl into the DOM
 
 
     },
