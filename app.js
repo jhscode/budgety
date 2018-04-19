1a7ed87290522e6c27c0465d9066ebe76606188c
James Shin <jhscode@gmail.com>
2018-04-19 07:56:14 -0400

    added insertAdjacentHTML to addlist fn; updated crtadditem fn to update UI

--- app.js
+++ app.js
@@ -64,7 +64,9 @@ var UIController = (function() {
     inputType: '.add__type',
     inputDescription: '.add__description',
     inputValue: '.add__value',
-    inputBtn: '.add__btn'
+    inputBtn: '.add__btn',
+    incomeContainer: '.income__list',
+    expensesContainer: '.expenses__list'
   };
 
   return {
@@ -77,16 +79,20 @@ var UIController = (function() {
     },
 
     addListItem: function(obj, type) {
-      var html, newHTML;
+      var html, newHTML, element;
 
       // Create HTML string with placeholder text
       if (type === 'inc') {
+        element =  DOMstrings.incomeContainer;
+
         html = '<div class="item clearfix" id="income-%id%"><div class="item__description">'+
         '%description%</div><div class="right clearfix"><div class="item__value">%value%</div>'+
         '<div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline">'+
         '</i></button></div></div></div>';
 
       } else if (type === 'exp') {
+        element = DOMstrings.expensesContainer;
+
         html = '<div class="item clearfix" id="expense-%id%"><div class="item__description">%description%</div>'+
         '<div class="right clearfix"><div class="item__value">%value%</div><div class="item__percentage">'+
         '21%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i>'+
@@ -97,9 +103,9 @@ var UIController = (function() {
       newHTML = html.replace('%id%',obj.id);
       newHTML = newHTML.replace('%description%', obj.description);
       newHTML = newHTML.replace('%value%', obj.value);
-      
-      // Insert HTMl into the DOM
 
+      // Insert HTMl into the DOM
+      document.querySelector(element).insertAdjacentHTML('beforeend', newHTML);
 
     },
 
@@ -139,7 +145,8 @@ var controller = (function(budgetCtrl, UICtrl) {
         newItem = budgetCtrl.addItem(input.type, input.description, input.value);
 
         // 3. Add the item to the UI
-
+        UICtrl.addListItem(newItem, input.type);
+        
         // 4. Calculate the budget
 
         // 5. Display the budget on the UI
