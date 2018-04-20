be5ad1fb085115b2de2202f4768416a9707c6a3c
James Shin <jhscode@gmail.com>
2018-04-20 07:17:00 -0400

    convert input value to int, template of update budget func

--- app.js
+++ app.js
@@ -74,7 +74,7 @@ var UIController = (function() {
       return {
         type: document.querySelector(DOMstrings.inputType).value, // Will be either inc or exp
         description: document.querySelector(DOMstrings.inputDescription).value,
-        value: document.querySelector(DOMstrings.inputValue).value
+        value: parseFloat(document.querySelector(DOMstrings.inputValue).value);
       };
     },
 
@@ -147,6 +147,14 @@ var controller = (function(budgetCtrl, UICtrl) {
     });
   };
 
+  var updateBudget = function() {
+
+        // 1. Calculate the budget
+
+        // 2. Return the budget
+
+        // 3. Display the budget on the UI
+  };
 
   // Adding items
   var ctrlAddItem = function() {
@@ -164,9 +172,8 @@ var controller = (function(budgetCtrl, UICtrl) {
         // 4. Clear the fields
         UICtrl.clearFields();
 
-        // 5. Calculate the budget
-
-        // 6. Display the budget on the UI
+        // 5. Calculate and update budget
+        updateBudget();
 
   };
 
