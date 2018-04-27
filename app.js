17751769a659b8871de1fc26a6266b35e715f94f
James Shin <jhscode@gmail.com>
2018-04-27 07:46:49 -0400

    added red/green border on UI

--- app.js
+++ app.js
@@ -180,6 +180,12 @@ var UIController = (function() {
     return (type === 'exp' ? '-' : '+') + ' ' + int + '.' + dec;
   };
 
+  var nodeListForEach = function(list, callback) {
+    for (var i = 0; i < list.length; i++) {
+      callback(list[i], i);
+    }
+  };
+
   return {
     getInput: function() {
       return {
@@ -262,12 +268,6 @@ var UIController = (function() {
     displayPercentages: function(percentages) {
         var fields = document.querySelectorAll(DOMstrings.expensesPercLabel);
 
-        var nodeListForEach = function(list, callback) {
-          for (var i = 0; i < list.length; i++) {
-            callback(list[i], i);
-          }
-        };
-
       nodeListForEach(fields, function(current, index) {
         if (percentages[index] > 0){
           current.textContent = percentages[index] + '%';
@@ -289,6 +289,22 @@ var UIController = (function() {
       document.querySelector(DOMstrings.dateLabel).textContent = months[month] + ' ' + year;
     },
 
+    changedType: function() {
+
+        var fields = document.querySelectorAll(
+          DOMstrings.inputType + ',' +
+          DOMstrings.inputDescription + ',' +
+          DOMstrings.inputValue
+        );
+
+        nodeListForEach(fields, function(cur) {
+          cur.classList.toggle('red-focus');
+        });
+
+        document.querySelector(DOMstrings.inputBtn).classList.toggle('red');
+
+    },
+
     getDOMstrings: function() {
       return DOMstrings;
     }
@@ -304,6 +320,8 @@ var controller = (function(budgetCtrl, UICtrl) {
     var DOM = UICtrl.getDOMstrings();
 
     document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
+
+    document.querySelector(DOM.inputType).addEventListener('change', UICtrl.changedType);
 
     document.addEventListener('keypress', function(event) {
       if (event.keyCode === 13 || event.which === 13) {
